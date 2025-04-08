42f92b27c001f5ecb88d663ac7a84d0412531855
T1mmmmm <wy2135@nyu.edu>
2025-04-08 02:34:17 -0400

    chore: add module docstring to fix pylint CI

--- machine-learning-client/main.py
+++ machine-learning-client/main.py
@@ -1,2 +1,3 @@
+"""Main script for the machine learning client. Handles audio input and mood detection."""
 if __name__ == "__main__":
     print("ML client container started. Replace with real logic lator.")
